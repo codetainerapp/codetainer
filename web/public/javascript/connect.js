3d4db444a4ba02f2b723088dc2f890b73a117269
Dustin Willis Webber <dustin.webber@gmail.com>
2015-06-28 19:37:15 -0400

    update

--- web/public/javascript/connect.js
+++ web/public/javascript/connect.js
@@ -3,20 +3,96 @@ var Codetainer;
 function resize(term) {
   console.log("IN RESIZE");
 
+  // var x = $(".terminal").width();
+  // var y = $('.terminal').height();
+
   var x = document.body.clientWidth / term.element.offsetWidth;
   var y = document.body.clientHeight / term.element.offsetHeight;
-
-  console.log("word", x,y, $('body').height(), $("body").width())
 
   x = x * term.cols | 0;
   y = y * term.rows | 0;
 
-  term.resize(x, y);
+  // x = x * term.cols | 0;
+  // y = y * term.rows | 0;
+
+  console.log("word", x,y, $('body').height(), $("body").width())
+
+  Codetainer.Resize(x,y, function() {
+    term.resize(x, y);
+  });
 }
 
 Codetainer = {
+  id: "",
+
+  Ajax: {
+    Cache: {},
+
+    Fetch: function(opts, callback, errback) {
+
+      if (Codetainer.Ajax.Cache.hasOwnProperty(opts.url)) {
+        Codetainer.Ajax.Cache[opts.url].abort();
+      }
+
+      var options = {
+        dataType: "json",
+        success: function(data) {
+          delete Codetainer.Ajax.Cache[options.url]
+
+          if (callback && typeof callback === "function") {
+            return callback(data)
+          } else {
+            console.log(data);
+          }
+        },
+        error: function(a, b, c) {
+          delete Codetainer.Ajax.Cache[options.url]
+
+          if (errback && typeof errback === "function") {
+            return errback(a, b, c);
+          } else {
+            console.log(a, b, c)
+          }
+        }
+      };
+
+      $.extend(opts, options)
+
+      Codetainer.Ajax.Cache[opts.url] = $.ajax(opts)
+    },
+
+    error: function(a, b, c) {
+      console.log(a, b, c);
+    }
+  },
+
+  Resize: function(x, y, callback) {
+    console.log("Woprd?")
+    Codetainer.Ajax.Fetch({
+      url: "/api/v1/codetainer/" + Codetainer.id + "/tty",
+      data: {
+        height: y,
+        width: x
+      },
+      dataType: "json",
+      type: "post"
+    }, function(data) {
+      console.log("HJELLLLO", data);
+
+      if (callback && typeof callback === "function") {
+        return callback(data);
+      }
+    }, function(a,b,c,d) {
+      console.log("WTF", a,b,c,d)
+
+      if (callback && typeof callback === "function") {
+        // return callback();
+      }
+    });
+  },
 
   Build: function(container) {
+    this.id = container
 
     var term = new Terminal({});
 
