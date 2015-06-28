d1a351d3a13b06d9cac533b6e9dc70052b2a0a13
Jen Andre <jandre@gmail.com>
2015-06-28 15:35:51 -0400

    use komanda for testing

--- container.go
+++ container.go
@@ -1,1 +1,74 @@
 package main
+
+import (
+	"net"
+	"net/http"
+	"net/url"
+
+	"github.com/gorilla/websocket"
+)
+
+type ContainerConnection struct {
+	id        string
+	web       *websocket.Conn
+	container *websocket.Conn
+}
+
+func (c *ContainerConnection) read() {
+	for {
+		_, message, err := c.web.ReadMessage()
+		if err != nil {
+			break
+		}
+		c.container.WriteMessage(websocket.TextMessage, message)
+	}
+	c.web.Close()
+}
+
+func (c *ContainerConnection) write() {
+	for {
+		_, message, err := c.container.ReadMessage()
+		if err != nil {
+			break
+		}
+		c.web.WriteMessage(websocket.TextMessage, message)
+	}
+	c.container.Close()
+}
+
+func (c *ContainerConnection) Start() error {
+
+	if client, err := newContainerClient(c.id); err != nil {
+		return err
+	} else {
+		c.container = client
+	}
+
+	go c.read()
+	c.write()
+	return nil
+}
+
+func newContainerClient(id string) (*websocket.Conn, error) {
+	//id := "9a58ba6db179"
+	u, err := url.Parse("http://komanda.io:4500/v1.5/containers/" + id + "/attach/ws?logs=1&stderr=1&stdout=1&stream=1&stdin=1")
+	if err != nil {
+		return nil, err
+	}
+
+	rawConn, err := net.Dial("tcp", u.Host)
+	if err != nil {
+		return nil, err
+	}
+
+	wsHeaders := http.Header{
+		"Origin": {"http://localhost:4500"},
+	}
+
+	wsConn, resp, err := websocket.NewClient(rawConn, u, wsHeaders, 1024, 1024)
+
+	if err != nil {
+		Log.Error("Unable to connect", resp, err)
+	}
+	return wsConn, err
+}
--- http-handlers.go
+++ http-handlers.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"errors"
-	"net"
-	"net/http"
-	"net/url"
 
 	"github.com/gorilla/mux"
-	"github.com/gorilla/websocket"
 )
 
 func RouteIndex(ctx *Context) error {
@@ -16,77 +12,20 @@ func RouteIndex(ctx *Context) error {
 	})
 }
 
-type ContainerConnection struct {
-	web       *websocket.Conn
-	container *websocket.Conn
-}
-
-func (c *ContainerConnection) read() {
-	for {
-		_, message, err := c.web.ReadMessage()
-		if err != nil {
-			break
-		}
-		c.container.WriteMessage(websocket.TextMessage, message)
-	}
-	c.web.Close()
-}
-
-func (c *ContainerConnection) write() {
-	for {
-		_, message, err := c.container.ReadMessage()
-		if err != nil {
-			break
-		}
-		c.web.WriteMessage(websocket.TextMessage, message)
-	}
-	c.container.Close()
-}
-
-func (c *ContainerConnection) Start() {
-	go c.read()
-	c.write()
-}
-
-func newContainerClient(id string) (*websocket.Conn, error) {
-	//id := "9a58ba6db179"
-	u, err := url.Parse("http://localhost:4500/v1.5/containers/" + id + "/attach/ws?logs=1&stderr=1&stdout=1&stream=1&stdin=1")
-	if err != nil {
-		return nil, err
-	}
-
-	rawConn, err := net.Dial("tcp", u.Host)
-	if err != nil {
-		return nil, err
-	}
-
-	wsHeaders := http.Header{
-		"Origin": {"http://localhost:4500"},
-	}
-
-	wsConn, resp, err := websocket.NewClient(rawConn, u, wsHeaders, 1024, 1024)
-
-	if err != nil {
-		Log.Error("Unable to connect", resp, err)
-	}
-	return wsConn, err
-}
-
 func RouteApiV1CodetainerAttach(ctx *Context) error {
 	vars := mux.Vars(ctx.R)
 	id := vars["id"]
-	connection := &ContainerConnection{}
 
-	if c, err := newContainerClient(id); err != nil {
-		return err
-	} else {
-		connection.container = c
+	if id == "" {
+		return errors.New("ID of container must be provided")
 	}
 
 	if ctx.WS == nil {
 		return errors.New("No websocket connection for web client")
 	}
-	connection.web = ctx.WS
+
+	connection := &ContainerConnection{id: id, web: ctx.WS}
+
 	connection.Start()
 
 	return nil
