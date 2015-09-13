c750ba4e564352246e1c56ba6e5109ed1f63eed2
Jen Andre <jandre@gmail.com>
2015-09-13 16:00:11 -0400

    update spec to use opencontainer spec

--- spec.go
+++ spec.go
@@ -1,41 +1,30 @@
 package codetainer
 
-type Mount struct {
-	Type        string `json:"type"`
-	Source      string `json:"source"`
-	Destination string `json:"destination"`
-	Options     string `json:"options"`
-}
+import (
+	"encoding/json"
+	"fmt"
+	"os"
 
-type Process struct {
-	TTY  bool     `json:"tty"`
-	User string   `json:"user"`
-	Args []string `json:"args"`
-	Env  []string `json:"env"`
-	Cwd  string   `json:"cwd"`
-}
+	"github.com/opencontainers/specs"
+)
 
-type Root struct {
-	Path     string `json:"path"`
-	Readonly bool   `json:"readonly"`
-}
-
-type Namespace struct {
-	Type string `json:"type"`
-	Path string `json:"path,omitempty"`
-}
-
-type PortableSpec struct {
-	Version      string      `json:"version"`
-	OS           string      `json:"os"`
-	Arch         string      `json:"arch"`
-	Processes    []*Process  `json:"processes"`
-	Root         Root        `json:"root"`
-	Cpus         float64     `json:"cpus"`   // in 1.1 for 110% cpus
-	Memory       int64       `json:"memory"` // in mb; 1024m
-	Hostname     string      `json:"hostname"`
-	Namespaces   []Namespace `json:"namespaces"`
-	Capabilities []string    `json:"capabilities"`
-	Devices      []string    `json:"devices"`
-	Mounts       []Mount     `json:"mounts"`
+// loadSpec loads the specification from the provided path.
+// If the path is empty then the default path will be "config.json"
+func loadSpec(path string) (*specs.LinuxRuntimeSpec, error) {
+	if path == "" {
+		path = "config.json"
+	}
+	f, err := os.Open(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, fmt.Errorf("JSON specification file for %s not found", path)
+		}
+		return nil, err
+	}
+	defer f.Close()
+	var s *specs.LinuxRuntimeSpec
+	if err := json.NewDecoder(f).Decode(&s); err != nil {
+		return nil, err
+	}
+	return s, nil
 }
